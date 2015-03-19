5af4b8895bdf117ee72515eba61c1e7f7dedbb0d
Mykola Dvornik <mykola.dvornik@gmail.com>
2015-03-19 14:03:09 +0100

    add mxyz2muvw(...) function that (a) extracts dynamic component of the magnetization vector (b) transforms it to the uvw coordinates as given by the local magnetization direction in ground state

---
+++ core/preprocess.c
@@ -0,0 +1,94 @@
+#include "core/preprocess.h"
+
+void mxyz2muvw(float **data, float *m0, size_t comp_size, size_t number_of_frames) {
+
+	size_t i, k, bias;
+
+	float *m = *data;
+	float *mx;
+	float *my;
+	float *mz;
+
+	float *mu;
+	float *mv;
+	float *mw;
+
+	float *m0x = &m0[0 * comp_size];
+	float *m0y = &m0[1 * comp_size];
+	float *m0z = &m0[2 * comp_size];
+
+	float dmx, dmy, dmz, dmu, dmv, dmw;
+	float ux, uy, uz;
+	float vx, vy, vz;
+	float wx, wy, wz;
+	float nu, nv, nw;
+
+	for (k = 0; k < number_of_frames; k++) {
+
+		bias = 3 * comp_size * k;
+
+		mx = &m[0 * comp_size + bias];
+		my = &m[1 * comp_size + bias];
+		mz = &m[2 * comp_size + bias];
+
+		mu = mx;
+		mv = my;
+		mw = mz;
+
+		for (i = 0; i < comp_size; i++)	{
+
+			// calculate dynamic part of the magnetization
+			dmx = mx[i] - m0x[i];
+			dmy = my[i] - m0y[i];
+			dmz = mz[i] - m0z[i];
+
+			// go to native, uvw, coordinate system
+
+			// u coincides with the magnetization direction in ground state
+			ux = m0x[i];
+			uy = m0y[i];
+			uz = m0z[i];
+
+			nu = norm(ux, uy, uz);
+			if (nu != 0.0f) {
+				ux /= nu;
+				uy /= nu;
+				uz /= nu;
+			}
+
+			// v should be in-plane (won't work for non-planar geometries)
+			vx = -uy;
+			vy = ux;
+			vz = 0.0f;
+
+			nv = norm(vx, vy, vz);
+			if (nv != 0.0f) {
+				vx /= nv;
+				vy /= nv;
+				vz /= nv;
+			}
+
+			// w = u × v
+
+			wx = (uy * vz - uz * vy);
+			wy = (uz * vx - ux * vz);
+			wz = (ux * vy - uy * vx);
+
+			nw = norm(wx, wy, wz);
+			if (nw != 0.0f) {
+				wx /= nw;
+				wy /= nw;
+				wz /= nw;
+			}
+
+			dmu = dot(dmx, dmy, dmz, ux, uy, uz);
+			dmv = dot(dmx, dmy, dmz, vx, vy, vz);
+			dmw = dot(dmx, dmy, dmz, wx, wy, wz);
+
+			mu[i] = dmu;
+			mv[i] = dmv;
+			mw[i] = dmw;
+		}
+	}
+	return;
+}
---
+++ core/preprocess.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "common.h"
+
+void mxyz2muvw(float **data, float *m0, size_t comp_size, size_t number_of_frames);
