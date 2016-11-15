3ee203442c85cb436146177a666e85a09d90af1d
Mykola 'godsic' Dvornik <mykola.dvornik@gmail.com>
2016-11-15 13:52:19 +0100

    core: switch to threaded (openmp) version of FFTW

--- core/fft.c
+++ core/fft.c
@@ -1,1 +1,8 @@
 #include "core/fft.h"
+
+int init_threaded_fftw()
+{
+    int err = fftwf_init_threads();
+    fftwf_plan_with_nthreads(omp_get_max_threads());
+    return err;
+}
--- core/fft.h
+++ core/fft.h
@@ -1,3 +1,6 @@
 #pragma once
 
 #include <fftw3.h>
+#include <omp.h>
+
+int init_threaded_fftw();
